b0c118807a41bd515547b240603395fbb56db529
Anjali <anjalipndey06@gmail.com>
2025-11-11 03:03:20 +0530

    login&Profile

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import assets, { imagesDummyData } from '../assets/assets'
 
 const RightSidebar = ({selectedUser}) => {
-  return (
+  return  selectedUser && (
     <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden" : "" }`}>
 
       <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto' >
@@ -26,8 +26,9 @@ const RightSidebar = ({selectedUser}) => {
         ))}
        </div>
       </div>
+      <button className='absolute bottom-5 left-1/2 tranform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none texxt-sm font-light py-2 px-20 rounded-full cursor-pointer '>Logout</button>
     </div>
   )
-}
+} 
+export default RightSidebar;
 
-export default RightSidebar
